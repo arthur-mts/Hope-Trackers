2dc07d5b611915d3a991ec594272a60f7b5e9b9c
jarthursantos <silva.arthur@academico.ifpb.edu.br>
2020-04-23 10:07:26 -0300

    refact: remove unnecessary files

--- mobile/ios/hopetrackersTests/hopetrackersTests.m
+++ mobile/ios/hopetrackersTests/hopetrackersTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface HopeTrackersTests : XCTestCase
+@interface hopetrackersTests : XCTestCase
 
 @end
 
-@implementation HopeTrackersTests
+@implementation hopetrackersTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
