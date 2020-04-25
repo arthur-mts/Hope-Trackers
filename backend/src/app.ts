d0b1a7ca83d020a649358c40df8152f27f8b9b59
punisher077 <arthur.mts@gmail.com>
2020-04-25 12:03:47 -0300

    feat: 🎸 add multer config and file config to express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,34 +1,33 @@
-import express, {Application} from 'express';
+import express, { Application } from 'express';
 import mongoose from 'mongoose';
-
 import router from './routes';
-
+import path from 'path';
 class App {
-    public app: Application;
-    constructor() {
-        this.app = express();
-        this.setConfig();
-        this.setMongoConfig();
-        this.setRoutes();
-        this.app.listen(8000);
-    }
+  public app: Application;
+  constructor() {
+    this.app = express();
+    this.setConfig();
+    this.setMongoConfig();
+    this.setRoutes();
+    this.app.listen(8000);
+  }
 
-    private setRoutes() {
-        console.log('gere');
-        this.app.use('/', router);
-    }
+  private setRoutes() {
+    this.app.use(router);
+  }
 
-    private setMongoConfig() {
-        mongoose.Promise = global.Promise;
-        mongoose.connect('mongodb://localhost:27017/HopeTrackers', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-    }
+  private setMongoConfig() {
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost:27017/HopeTrackers', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
 
-    private setConfig() {
-        this.app.use(express.json());
-    }
+  private setConfig() {
+    this.app.use(express.json());
+    this.app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+  }
 }
 
 export default new App().app;
