cd873257b969696b88a31bb03f6ed55713fd7d1a
punisher077 <arthur.mts@gmail.com>
2020-04-25 12:04:36 -0300

    feat: 🎸 finish company model

--- backend/src/models/company.ts
+++ backend/src/models/company.ts
@@ -1,17 +1,29 @@
 import mongoose from 'mongoose';
 import PointSchema from './util/point';
 
-const CompanyScheema: mongoose.Schema = new mongoose.Schema({
-    name: {type: String, required: true},
-    cnpj: {type: String, required: true},
-    description: {type: String, required: true},
-    phoneNumber: {type: String, required: true},
+const CompanyScheema: mongoose.Schema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    cnpj: { type: String, required: true },
+    description: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
     location: {
-        type: PointSchema,
-        index: '2dsphere',
+      type: PointSchema,
+      index: '2dsphere',
     },
     category: String,
     schedule: String,
+    thunbnail: String,
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+CompanyScheema.virtual('thunbnail_url').get(function (this: { thunbnail: String }) {
+  return `https://localhost:8000/files/${this.thunbnail}`;
 });
 
 export const Company = mongoose.model('Company', CompanyScheema);
