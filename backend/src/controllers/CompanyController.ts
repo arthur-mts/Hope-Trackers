9a58d9b9b3ca6402935923299177c990e9f28b29
punisher077 <arthur.mts@gmail.com>
2020-04-25 12:04:08 -0300

    fix: 🐛 fix cnpj and cpf validation

--- backend/src/controllers/CompanyController.ts
+++ backend/src/controllers/CompanyController.ts
@@ -1,42 +1,34 @@
-import {Request, Response} from 'express';
-import {Company} from '../models/company';
-import {validator} from 'cpf-cnpj-validator';
+import { Request, Response } from 'express';
+import { Company } from '../models/company';
+import { cnpj as cnpjUtil } from 'cpf-cnpj-validator';
+
 export default class CompanyController {
-    public static async store(req: Request, res: Response) {
-        const {
-            name,
-            cnpj,
-            latitude,
-            longitude,
-            description,
-            phoneNumber,
-            category,
-        } = req.body;
-        console.log(validator('123'));
-        return res.status(400).send({message: 'Invalid CNPJ'});
-        if (true) {
-            const location = {
-                type: 'Point',
-                coordinates: [longitude, latitude],
-            };
+  public static async store(req: Request, res: Response) {
+    console.log(req.body);
+    const { filename } = req.file;
+    const { name, cnpj, latitude, longitude, description, phoneNumber, category } = req.body;
+    console.log(cnpjUtil.isValid('123', false));
+    if (!cnpjUtil.isValid(cnpj, false)) return res.status(400).send({ mesage: 'Invalid CNPJ' });
 
-            let company = await Company.findOne({
-                phoneNumber,
-            });
+    const location = {
+      type: 'Point',
+      coordinates: [longitude, latitude],
+    };
 
-            if (company)
-                return res.status(400).send({mesage: 'Company alredy exists'});
-            else {
-                company = await Company.create({
-                    name,
-                    cnpj,
-                    description,
-                    phoneNumber,
-                    location,
-                    category,
-                });
-                return res.json(company);
-            }
-        }
-    }
+    let company = await Company.findOne({
+      phoneNumber,
+    });
+
+    if (company) return res.status(400).send({ mesage: 'Company alredy exists' });
+    company = await Company.create({
+      name,
+      thumbnail: filename,
+      cnpj,
+      description,
+      phoneNumber,
+      location,
+      category,
+    });
+    return res.json(company);
+  }
 }
--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,25 +1,23 @@
-import {Request, Response} from 'express';
-import {User} from '../models/user';
-import {cpf as cpfUtil} from 'cpf-cnpj-validator';
+import { Request, Response } from 'express';
+import { User } from '../models/user';
+import { cpf as cpfUtil } from 'cpf-cnpj-validator';
 
 export default class UserController {
-    public static async store(req: Request, res: Response) {
-        const {phoneNumber, name, cpf} = req.body;
-        if (cpfUtil.isValid(cpf, '-')) {
-            return res.status(400).send({message: 'Invalid CPF'});
-        }
+  public static async store(req: Request, res: Response) {
+    const { phoneNumber, name, cpf } = req.body;
+    if (!cpfUtil.isValid(cpf, false)) return res.status(400).send({ message: 'Invalid CPF' });
 
-        let user = await User.findOne({
-            phoneNumber,
-        });
-        if (user) return res.status(400).send({message: 'User alredy exists'});
-        else {
-            user = await User.create({
-                name,
-                phoneNumber,
-                cpf,
-            });
-            return res.json(user);
-        }
+    let user = await User.findOne({
+      phoneNumber,
+    });
+    if (user) return res.status(400).send({ message: 'User alredy exists' });
+    else {
+      user = await User.create({
+        name,
+        phoneNumber,
+        cpf,
+      });
+      return res.json(user);
     }
+  }
 }
