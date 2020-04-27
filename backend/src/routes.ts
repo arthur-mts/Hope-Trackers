e2a56e2ecff83aaac31de35d8ea8e872570fed3c
punisher077 <arthur.mts@gmail.com>
2020-04-27 12:20:19 -0300

    feat: 🎸 add favorite routes and auth middleware

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,13 +3,26 @@ import UserController from './controllers/UserController';
 import CompanyController from './controllers/CompanyController';
 import { SearchController } from './controllers/SearchController';
 import upload from './config/upload';
+import { SessionConotrller } from './controllers/SessionController';
+import auth from './middlewares/auth';
+import FavoriteController from './controllers/FavoriteController';
 
 const router = express.Router();
 
+router.post('/sessions', SessionConotrller.store);
+
+router.get('/companies', SearchController.index);
+
+router.use(auth);
+
 router.post('/users', UserController.store);
+
+router.put('/users', UserController.update);
 
 router.post('/companies', upload.single('thumbnail'), CompanyController.store);
 
-router.get('/companies', SearchController.index);
+router.post('/favorites', FavoriteController.store);
+
+router.get('/favorites', FavoriteController.index);
 
 export default router;
