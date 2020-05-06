f0813d09edf28e9a002274117ed1b5714b8550e5
punisher077 <arthur.mts@gmail.com>
2020-05-06 19:18:15 -0300

    refactor: 💡 update chat controller

--- backend/src/controllers/ChatController.ts
+++ backend/src/controllers/ChatController.ts
@@ -1,55 +1,52 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import Chat from '../models/chat';
-import {User} from "../models/user";
+import { User } from "../models/user";
 import mongoose from 'mongoose'
-import {Company} from "../models/company";
 
 class ChatController {
-  public async store(req: Request, res: Response){
-    const {user_id} = req.body;
+  public async store(req: Request, res: Response) {
+    const { user_id } = req.body;
 
     const destiny = mongoose.Types.ObjectId(req.params.destiny);
 
-    let chat = await Chat.findOne({users : {$all:  [user_id, destiny]}});
+    let chat = await Chat.findOne({ users: { $all: [user_id, destiny] } });
 
-    if(chat) return res.status(400).send({message: 'Chat alredy  exists'});
+    if (chat) return res.status(400).send({ message: 'Chat alredy  exists' });
 
     chat = await Chat.create({ users: [user_id, destiny] });
-    
-   // chat.users.push(user_id, destiny);
-    
-   //  await chat.save();
 
-    await User.updateOne({_id: destiny}, { $push: { chats: chat }  });
+    // chat.users.push(user_id, destiny);
 
-    await User.updateOne({_id: user_id}, {$push: {chats: chat}});
+    //  await chat.save();
+
+    await User.updateOne({ _id: destiny }, { $push: { chats: chat } });
+
+    await User.updateOne({ _id: user_id }, { $push: { chats: chat } });
 
     return res.json(chat);
   }
 
-  public async index(req: Request, res: Response){
+  public async index(req: Request, res: Response) {
     const { user_id } = req.body;
 
-    const destiny  = mongoose.Types.ObjectId(req.params.destiny);
-    
-    const chat = await Chat.find({ users: {$all:  [user_id, destiny] }}).populate('messages');
-    
+    const destiny = mongoose.Types.ObjectId(req.params.destiny);
+
+    const chat = await Chat.find({ users: { $all: [user_id, destiny] } }).populate('messages');
+
     return res.json(chat);
   }
 
-  public async list(req: Request, res: Response){
-    const {user_id}= req.body;
+  public async list(req: Request, res: Response) {
+    const { user_id } = req.body;
 
-    let chats = await Chat.find({users: user_id }).populate('messages');
-    
-    chats  = await Promise.all( chats.map(async (chat)=>{
+    let chats = await Chat.find({ users: user_id }).populate('messages');
+
+    chats = await Promise.all(chats.map(async (chat) => {
       const reciverId = chat.users.filter((item, ) => {
         return item != user_id;
       })[0];
-      
-      let reciver : any = await User.findById(reciverId);
-    
-      if(!reciver) reciver = await Company.findById(reciverId);
+
+      const reciver: any = await User.findById(reciverId);
 
       chat.users[chat.users.indexOf(reciverId)] = reciver;
       return chat;
